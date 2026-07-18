c8fb1024bdaebc1b920893e07609f2add5cc1578
Joshua Washington <145852592+G4l1le30@users.noreply.github.com>
2026-07-18 11:36:13 +0700

    fix: VERCEL env var untuk dynamic base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/nextgenreact/',
+  base: process.env.VERCEL ? '/' : '/nextgenreact/',
   test: {
     globals: true,
     environment: 'jsdom',
